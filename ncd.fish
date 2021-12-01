3275133a7de6a0f21bfb3f48623a913c338e7600
James Sully <sullyj3@gmail.com>
2021-12-01 15:28:28 +1100

    Add todo

--- ncd.fish
+++ ncd.fish
@@ -1,3 +1,5 @@
+# todo: don't cd if nothing is selected
+
 function ncd --wraps='cd (nearby|head -n 100|fzf)' --description 'alias ncd=cd (nearby|head -n 100|fzf)'
   cd (nearby|head -n 10000|fzf) $argv; 
 end
