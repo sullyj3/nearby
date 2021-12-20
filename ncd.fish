f63d26f08025f7d71e5249410492c8d999e2b2b8
James Sully <sullyj3@gmail.com>
2021-12-20 20:48:09 +1100

    Make fish script nicer

--- ncd.fish
+++ ncd.fish
@@ -1,5 +1,15 @@
 # todo: don't cd if nothing is selected
 
 function ncd --wraps='cd (nearby|head -n 100|fzf)' --description 'alias ncd=cd (nearby|head -n 100|fzf)'
-  cd (nearby|head -n 10000|fzf) $argv; 
+  if count $argv &> /dev/null;
+    set TARGET (nearby|head -n 10000|fzf -q $argv);
+  else;
+    set TARGET (nearby|head -n 10000|fzf);
+  end
+
+  if count $TARGET &> /dev/null;
+    cd $TARGET
+  else;
+    echo "No target selected!"
+  end
 end
